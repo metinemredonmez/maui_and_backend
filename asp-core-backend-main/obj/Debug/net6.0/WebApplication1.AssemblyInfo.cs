f50516215db77920f051fd1b80f92faef4a3cacb
Metin Emre <metinemredonmez@hotmail.com>
2023-10-03 17:31:48 +0300

    second
    updated

--- asp-core-backend-main/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ asp-core-backend-main/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c44b479a44cf9f526a30af8186a6866df9d012")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
